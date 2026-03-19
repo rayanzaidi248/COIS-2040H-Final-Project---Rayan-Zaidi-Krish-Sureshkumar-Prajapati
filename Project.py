7f937ebbd757f2222f730422c93e9495d940ee47
Seyjal Guda <123223222+seyjal14@users.noreply.github.com>
2026-03-19 17:11:54 -0400

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,3 +1,3 @@
 #Final Project Assignment
 #Restaurant Reservation System
-#Group Members: Rayan Zaidi & Krish Sureshkumar Prajapati
+#Group Members: Rayan Zaidi, Krish Sureshkumar Prajapati & Seyjal Kaushik Guda
